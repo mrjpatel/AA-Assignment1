73cd2fdd487bf9626b75bad528254adf62e502be
rmit-s3603075-danny-ho <s3603075@student.rmit.edu.au>
2017-08-12 15:04:45 +1000

    bst add

--- src/BstMultiset.java
+++ src/BstMultiset.java
@@ -6,11 +6,49 @@ public class BstMultiset<T> extends Multiset<T>
 	
 	public BstMultiset() {
 		mHead = null;
-		// Implement me!
 	} // end of BstMultiset()
 
 	public void add(T item) {
-		// Implement me!
+		
+		Node<T> currentNode;
+		Node<T> newNode;
+		
+		if(mHead == null)	{
+			mHead = new Node<T>(item);
+			return;
+		}
+		
+		currentNode = mHead;
+		
+		while(true)	{
+			//If item the same, increase count
+			if(currentNode.compareTo(item) == 0)	{
+				currentNode.setCount(currentNode.getCount() +1);
+				return;
+			}
+			
+			if(currentNode.compareTo(item) == -1)	{
+				if(currentNode.getLeft() == null)	{
+					newNode = new Node<T>(item);
+					currentNode.setLeft(newNode);
+					return;
+				} else	{
+					currentNode = currentNode.getLeft();
+					continue;
+				}
+			}
+			
+			if(currentNode.compareTo(item) == 1)	{
+				if(currentNode.getRight() == null)	{
+					newNode = new Node<T>(item);
+					currentNode.setRight(newNode);
+					return;
+				} else	{
+					currentNode = currentNode.getRight();
+				}
+			}
+		}
+		
 	} // end of add()
 
 
@@ -36,10 +74,11 @@ public class BstMultiset<T> extends Multiset<T>
 		// Implement me!
 	} // end of print()
 	
-	private class Node<T>
+	private class Node<T> implements Comparable<T>
 	{
 		/** Value of node. */
 	    private T mValue;
+	    private int count = 1;
 	    /** Reference to next node. */
 	    private Node<T> mLeft;
 	    /** Reference to previous node. */
@@ -77,6 +116,19 @@ public class BstMultiset<T> extends Multiset<T>
 	    public void setRight(Node<T> right) {
 	        this.mRight = right;
 	    }
+
+		@Override
+		public int compareTo(T item) {
+			return this.mValue.toString().compareTo(item.toString());
+		}
+
+		public int getCount() {
+			return count;
+		}
+
+		public void setCount(int count) {
+			this.count = count;
+		}
 	} // end of inner class Node
 
 } // end of class BstMultiset
