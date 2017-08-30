604eff8c570c3c46a5cf07bf073e97fb22e3a652
rmit-s3603075-danny-ho <s3603075@student.rmit.edu.au>
2017-08-31 00:37:46 +1000

    datagen fix for search scenario

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -7,7 +7,7 @@ public class DataGenerator {
 	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		if (args.length != 3) {
+		if (args.length != 4) {
 			inputs();
 		}
 		
@@ -22,8 +22,29 @@ public class DataGenerator {
 		double numOfScen = Double.parseDouble(args[1]) /100;
 		int indexes = (int)Math.floor(numOfScen * numOfStr);
 		
-		
 		randStringArr(rand, numOfStr);
+		
+		
+		//Search
+		
+		ArrayList<String> randSc = new ArrayList<String>();
+		
+		if(args[3] != null)	{
+			for(String i : rand) {
+				randomScenGen(randSc, i, args[2]);
+			}
+			double numOfSea = Double.parseDouble(args[3]) /100;
+			int indexSea = (int)Math.floor(numOfSea * numOfStr);
+			Random rnd = new Random();
+			for(int i = 0; i < indexSea; i++)	{
+				String x = rand.get(rnd.nextInt(rand.size()));
+				randomSeaGen(randSc, x);
+			}
+		}
+		
+		Collections.shuffle(randSc);
+		
+		//end
 		
 		try {
 			PrintWriter writer = new PrintWriter(fileNameAdd);
@@ -32,7 +53,14 @@ public class DataGenerator {
 			if(args[2].equalsIgnoreCase("a"))	{
 				writer.close();
 			} else	{
-				printScenario(rand, writer, args[2], indexes);
+				if(args[3] != null)	{
+					for(String i : randSc)	{
+				    	writer.println(i);
+					}
+				}
+				else	{
+					printScenario(rand, writer, args[2], indexes);
+				}
 				writer.close();
 			}
 		} catch (IOException e) {
@@ -43,24 +71,51 @@ public class DataGenerator {
 	}
 	
 	public static void inputs()	{
-		System.out.println("Arguments: <num of strings to gen> <percentage of removals> <scenario>");
-		System.out.println("i.e. 100 75 S");
+		System.out.println("Arguments: <num of strings to gen> <percentage of removals> <scenario> <percentage of searches>");
+		System.out.println("i.e. 100 75 S 200");
 		System.out.println("Possible scenarios:A, S, RO, RA");
-		System.exit(0);
 	}
 	
 	//Random Generators
 	
 	public static String randomStringGen()	{
 		String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        StringBuilder salt = new StringBuilder();
+        StringBuilder randstr = new StringBuilder();
         Random rnd = new Random();
-        while (salt.length() < 18) {
+        while (randstr.length() < 18) {
             int index = (int) (rnd.nextFloat() * chars.length());
-            salt.append(chars.charAt(index));
+            randstr.append(chars.charAt(index));
         }
-        String saltStr = salt.toString();
-        return saltStr;
+        String str = randstr.toString();
+        return str;
+	}
+	
+	//Hack functions for search...
+	
+	/*public static void randomScenGen(ArrayList<String> list, ArrayList<String> dest, String scen)	{
+        StringBuilder randstr = new StringBuilder();
+        for(String i : list)	{
+        	randstr.append(scen + " ");
+        	randstr.append(i);
+        	String str = randstr.toString();
+        	dest.add(str);
+		}
+  	}*/
+	
+	public static void randomScenGen(ArrayList<String> dest, String key, String scen)	{
+		StringBuilder randstr = new StringBuilder();
+		randstr.append(scen + " ");
+    	randstr.append(key);
+    	String str = randstr.toString();
+    	dest.add(str);
+	}
+	
+	public static void randomSeaGen(ArrayList<String> dest, String key)	{
+		StringBuilder randstr = new StringBuilder();
+		randstr.append("S ");
+    	randstr.append(key);
+    	String str = randstr.toString();
+    	dest.add(str);
 	}
 	
 	public static void randStringArr(ArrayList<String> list, int numOfStr)	{
@@ -86,5 +141,5 @@ public class DataGenerator {
 			writer.print(scenario + " ");
 			writer.println(list.get(i));
 		}
-	}	
+	}
 }
